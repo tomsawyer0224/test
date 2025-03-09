7053747ab755474ef3f65ff35953bd86c59135b5
tomsawyer0224 <binha0224@gmail.com>
2025-03-09 10:14:43 +0700

    update test from dev branch

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pypdf
 import os
-import pdfreader
 print("this is test.py file")
 print('import successfully')
 print("this line comes from fix-connection branch")
-print("from new-feature branch created on local")
+print("from new-feature branch created on local host")
+print("from dev")
